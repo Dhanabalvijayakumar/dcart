ff067acad3100dbc0314a97059e8ed7232730767
Dhanabal V <dhanabalmailbox@gmail.com>
2026-06-01 23:16:54 +0530

    moved to brevo api

--- d-cart-backend/src/main/java/com/dhanabal/service/EmailServiceImpl.java
+++ d-cart-backend/src/main/java/com/dhanabal/service/EmailServiceImpl.java
@@ -1,40 +1,40 @@
 package com.dhanabal.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-	@Autowired
-	private JavaMailSender mailSender;
+	@Value("${BREVO_API_KEY}")
+	private String brevoApiKey;
 
 	@Override
 	public void sendRegistrationMail(String toEmail, String userName, String mobile, String password) {
 
-		SimpleMailMessage message = new SimpleMailMessage();
+		RestTemplate restTemplate = new RestTemplate();
 
-//		message.setFrom("vdhanabal2003@gmail.com");
-		message.setFrom("ad04f4001@smtp-brevo.com");
-		message.setTo(toEmail);
-		message.setSubject("Welcome to D'Cart");
+		String url = "https://api.brevo.com/v3/smtp/email";
 
-		message.setText("Hello " + userName + ",\n\n" + 
-						"Your D'Cart account has been created successfully.\n\n"+ 
-						"Now you can login and start shopping.\n\n" + 
-						"\n\n" + 
-						"Login Credentials:\n\n" + 
-						"Registered email: "+ toEmail + "\n\n" + 
-						"Registered mobile: "+mobile+"\n\n"+
-						"Password: " + password + "\n\n" +
-						"\n\n" + 
-						"Thank you,\n" + 
-						"Team D'Cart");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-		mailSender.send(message);
+		headers.set("api-key", brevoApiKey);
 
+		Map<String, Object> requestBody = Map.of("sender", Map.of("name", "DCart", "email", "vdhanabal2003@gmail.com"),
+				"to", List.of(Map.of("email", toEmail)), "subject", "Welcome to DCart", "htmlContent",
+				"<h2>Welcome to DCart</h2>" + "<p>Your account has been created successfully.</p>" + "<p><b>Email:</b> "
+						+ toEmail + "</p>" + "<p><b>Mobile:</b> " + mobile + "</p>"
+						+ "<p><b>Password:</b> " + password + "</p>"
+				);
+
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+		restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
 	}
-
 }
