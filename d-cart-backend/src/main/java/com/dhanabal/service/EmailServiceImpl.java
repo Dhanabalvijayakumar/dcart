47cdc0b987330e011676e53c073dfb7c4a83a8d3
Dhanabal V <dhanabalmailbox@gmail.com>
2026-06-01 23:32:30 +0530

    sender email changed

--- d-cart-backend/src/main/java/com/dhanabal/service/EmailServiceImpl.java
+++ d-cart-backend/src/main/java/com/dhanabal/service/EmailServiceImpl.java
@@ -26,7 +26,7 @@ public class EmailServiceImpl implements EmailService {
 
 		headers.set("api-key", brevoApiKey);
 
-		Map<String, Object> requestBody = Map.of("sender", Map.of("name", "DCart", "email", "vdhanabal2003@gmail.com"),
+		Map<String, Object> requestBody = Map.of("sender", Map.of("name", "DCart", "email", "dproductions8228@gmail.com"),
 				"to", List.of(Map.of("email", toEmail)), "subject", "Welcome to DCart", "htmlContent",
 				"<h2>Welcome to DCart</h2>" + "<p>Your account has been created successfully.</p>" + "<p><b>Email:</b> "
 						+ toEmail + "</p>" + "<p><b>Mobile:</b> " + mobile + "</p>"
